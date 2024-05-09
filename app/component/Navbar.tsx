fedac5b5efca3748cb79260048d3de683c948757
Toyehamzat <anibiseun@gmail.com>
2024-05-09 17:36:39 +0100

    chore: Add Navbar component for website navigation

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,46 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <nav className="text-[#ccd6f6] font-bold fixed w-full h-14 shadow-sm flex items-center">
+      <div className="md:max-w-screen-xl mx-auto flex items-center w-full justify-between">
+        <div className="w-2/5 flex space-x-8 ">
+          <div className="cursor-pointer text-xl tracking-widest font-extrabold">
+            Hamzat
+          </div>
+          <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
+            <div className=" transition hover:text-[#64ffda] cursor-pointer">
+              Home
+            </div>
+            <div className=" transition hover:text-[#64ffda] cursor-pointer">
+              About
+            </div>
+            <div className=" transition hover:text-[#64ffda] cursor-pointer">
+              Experience
+            </div>
+            <div className=" transition hover:text-[#64ffda] cursor-pointer">
+              Projects
+            </div>
+          </div>
+        </div>
+        <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
+          <Link href="mailto:anibiseun@gmail.com">
+            <Mail className="h-5 w-5 transition hover:text-[#64ffda] " />
+          </Link>
+          <Link href="https://github.com/Toyehamzat" target="_blank">
+            <Github className="h-5 w-5 transition hover:text-[#64ffda]" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/adetoye-anibi-32830823b"
+            target="_blank"
+          >
+            <Linkedin className="h-5 w-5 transition hover:text-[#64ffda]" />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
