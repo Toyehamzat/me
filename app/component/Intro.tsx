c735fa437f0bb7509aba8c3edd1bbcd46fe19134
Toyehamzat <anibiseun@gmail.com>
2024-05-09 17:37:04 +0100

    chore: Add Intro component for homepage rendering

---
+++ app/component/Intro.tsx
@@ -0,0 +1,11 @@
+import FractalTree from "./FractualTree";
+
+function Intro() {
+  return (
+    <div className="pt-52 flex flex-col text-center m-auto min-h-full md:max-w-screen-xl">
+      <FractalTree />
+    </div>
+  );
+}
+
+export default Intro;
