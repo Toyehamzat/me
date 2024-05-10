5e53875f39771c961f50c50ee2731e1bdc1f1cdc
Toyehamzat <anibiseun@gmail.com>
2024-05-10 17:23:07 +0100

    Refactor Intro component to remove unnecessary CSS class and improve performance

--- app/component/Intro.tsx
+++ app/component/Intro.tsx
@@ -7,7 +7,7 @@ const FractalTree = dynamic(() => import("@/app/component/FractualTree"), {
 
 function Intro() {
   return (
-    <div className="pt-52 flex flex-col text-center items-center m-auto min-h-full md:max-w-screen-xl">
+    <div className="flex flex-col text-center items-center m-auto min-h-full md:max-w-screen-xl">
       <FractalTree />
     </div>
   );
