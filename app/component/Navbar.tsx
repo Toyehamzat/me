7d4c786a111f06ff316d964a8ce5ad50c3bfc448
Toyehamzat <anibiseun@gmail.com>
2024-05-11 22:32:23 +0100

    refactor: Update Navbar styles for better responsiveness and spacing

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -9,12 +9,12 @@ function Navbar() {
         "text-[#ccd6f6]  font-bold fixed w-full h-14 shadow-sm flex items-center z-40"
       )}
     >
-      <div className="md:max-w-screen-xl mx-auto flex items-center w-full justify-between">
+      <div className="max-w-screen-xl mx-auto flex items-center w-full justify-between px-7 sm:px-4 md:px-0">
         <div className="w-2/5 flex space-x-8 ">
           <div className="cursor-pointer text-xl tracking-widest font-extrabold">
             Hamzat
           </div>
-          <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
+          <div className="space-x-4 md:w-auto hidden sm:flex items-center justify-between w-full ">
             <div className=" transition hover:text-[#64ffda] cursor-pointer">
               Home
             </div>
@@ -29,7 +29,7 @@ function Navbar() {
             </div>
           </div>
         </div>
-        <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
+        <div className="relative space-x-4 w-auto flex items-center justify-between">
           <Link href="mailto:anibiseun@gmail.com">
             <Mail className="h-5 w-5 transition hover:text-[#64ffda] " />
           </Link>
