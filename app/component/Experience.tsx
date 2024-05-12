9468c0a7ea358d60df676759692d33ea05047713
Toyehamzat <anibiseun@gmail.com>
2024-05-12 10:06:46 +0100

    experience component

---
+++ app/component/Experience.tsx
@@ -0,0 +1,16 @@
+import Reveal from "@/utils/Reaveal";
+import React from "react";
+
+function Experience() {
+  return (
+    <div className="min-h-[50vh] md:max-w-screen-xl pt-24">
+      <Reveal>
+        <div className="section-header">
+          <span className="section-title">/ Experience</span>
+        </div>
+      </Reveal>
+    </div>
+  );
+}
+
+export default Experience;
