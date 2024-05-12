46675d7008f925fa9c60cb891574994984ab4822
Toyehamzat <anibiseun@gmail.com>
2024-05-12 10:06:09 +0100

    globall css for header and titlee

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { TracingBeam } from "@/components/ui/tracing-beam";
 import Intro from "./component/Intro";
+import About from "./component/About";
+import Experience from "./component/Experience";
 
 export default function Home() {
   return (
     <TracingBeam className="px-6">
       <main className="pt-10">
         <Intro />
+        <About />
+        <Experience />
       </main>
     </TracingBeam>
   );
