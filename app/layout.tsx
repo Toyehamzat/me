efd92139f0e423ce2a4ce37bbe49ee4f5040b739
Toyehamzat <anibiseun@gmail.com>
2024-05-13 08:43:56 +0100

    refactor: Update body background color to #0a192f for better visual appeal

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={font.className} suppressHydrationWarning>
-      <body className=" bg-black ">
+      <body className=" bg-[#0a192f] ">
         <Navbar />
         {children}
       </body>
