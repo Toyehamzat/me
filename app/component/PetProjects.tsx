a1d408ff641822b6f62fa88c7a2f66f0ba1be20d
Toyehamzat <anibiseun@gmail.com>
2024-05-13 12:37:51 +0100

    init PET

---
+++ app/component/PetProjects.tsx
@@ -0,0 +1,16 @@
+import Reveal from "@/utils/Reaveal";
+import React from "react";
+
+function Pet() {
+  return (
+    <div className="min-h-[50vh] md:max-w-screen-xl pt-24">
+      <Reveal>
+        <div className="section-header">
+          <span className="section-title">/ Pet Projects</span>
+        </div>
+      </Reveal>
+    </div>
+  );
+}
+
+export default Pet;
