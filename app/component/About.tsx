38388977ae0cfab83c45feb2faf80380cb001f3d
Toyehamzat <anibiseun@gmail.com>
2024-05-13 08:44:04 +0100

    refactor: Update About component to remove location specificity in PinContainer title

--- app/component/About.tsx
+++ app/component/About.tsx
@@ -65,7 +65,7 @@ function About() {
             {two}
           </div>
           <div className="h-[20rem] w-full  items-center justify-center pl-9 hidden md:flex ">
-            <PinContainer title="Lagos,Nigeria." href="#">
+            <PinContainer title="Nigeria." href="#">
               <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
                   Aceternity UI ofc
