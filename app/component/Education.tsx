f076715e8f77f66cdea27f670a08f8b12ff4e502
Toyehamzat <anibiseun@gmail.com>
2024-05-13 16:04:07 +0100

    refactor: Update Education component layout to include bottom padding

--- app/component/Education.tsx
+++ app/component/Education.tsx
@@ -4,7 +4,7 @@ import { SchoolList } from "./SchoolList";
 
 function Education() {
   return (
-    <div className="min-h-[25vh] md:max-w-screen-xl pb-10 sm:pb-0">
+    <div className="min-h-[30vh] md:max-w-screen-xl pb-10 sm:pb-0">
       <Reveal>
         <div className="section-header">
           <span className="section-title">/ Education</span>
