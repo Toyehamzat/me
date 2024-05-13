6706989b1c4a2f0ef06e4d34d528313d7740d968
Toyehamzat <anibiseun@gmail.com>
2024-05-13 12:36:29 +0100

    added Education and PET component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { TracingBeam } from "@/components/ui/tracing-beam";
 import Intro from "./component/Intro";
 import About from "./component/About";
 import Experience from "./component/Experience";
+import Education from "./component/Education";
+import PetProjects from "./component/PetProjects";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
         <Intro />
         <About />
         <Experience />
+        <Education />
+        <PetProjects />
       </main>
     </TracingBeam>
   );
