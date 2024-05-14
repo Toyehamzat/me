5802d970f447ef1323383de67f032acf852a9f5e
Toyehamzat <anibiseun@gmail.com>
2024-05-14 16:20:15 +0100

    added a picture of idk who

--- app/component/About.tsx
+++ app/component/About.tsx
@@ -69,16 +69,14 @@ function About() {
           </div>
           <div className="h-[20rem] w-full  items-center justify-center pl-9 hidden md:flex ">
             <PinContainer title="Nigeria." href="#">
-              <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                  Aceternity UI ofc
-                </h3>
-                <div className="text-base !m-0 !p-0 font-normal">
-                  <span className="text-slate-500 ">
-                    a picture comming soon...
-                  </span>
-                </div>
-                <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-[#64ffda] to-[#e6f1ff]" />
+              <div className="flex basis-full flex-col tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                <div
+                  className="text-base !m-0 !p-0 font-normal bg-cover bg-center bg-no-repeat rounded-lg h-[20rem] w-[20rem]"
+                  style={{
+                    backgroundImage:
+                      'url("image/tim-mossholder-tq8Cuap8_wY-unsplash.jpg")',
+                  }}
+                ></div>
               </div>
             </PinContainer>
           </div>
