ab298fda534e31c4466871082673d692d9d31a57
Toyehamzat <anibiseun@gmail.com>
2024-05-14 07:16:42 +0100

    refactor: Add hover effect to PetProjects component

--- app/component/PetProjects.tsx
+++ app/component/PetProjects.tsx
@@ -1,15 +1,62 @@
 import Reveal from "@/utils/Reaveal";
 import React from "react";
 import { CarouselPlugin } from "./ImageSlider";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
 
 function Pet() {
+  const projects = [
+    {
+      title: "Stripe",
+      description:
+        "A technology company that builds economic infrastructure for the internet.",
+      link: "https://stripe.com",
+    },
+    {
+      title: "Netflix",
+      description:
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+      link: "https://netflix.com",
+    },
+    {
+      title: "Google",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "https://google.com",
+    },
+    {
+      title: "Meta",
+      description:
+        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+      link: "https://meta.com",
+    },
+    {
+      title: "Amazon",
+      description:
+        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+      link: "https://amazon.com",
+    },
+    {
+      title: "Microsoft",
+      description:
+        "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+      link: "https://microsoft.com",
+    },
+  ];
+
   return (
-    <div className="min-h-[50vh] md:max-w-screen-xl pt-24 pb-24">
+    <div className="min-h-[50vh] md:max-w-screen-xl pt-16 pb-24">
       <Reveal>
         <div className="section-header">
           <span className="section-title">/ Pet Projects</span>
         </div>
-        <CarouselPlugin />
+        <Reveal>
+          <CarouselPlugin />
+          <Reveal>
+            <div className=" mx-auto px-8 pt-5">
+              <HoverEffect items={projects} />
+            </div>
+          </Reveal>
+        </Reveal>
       </Reveal>
     </div>
   );
