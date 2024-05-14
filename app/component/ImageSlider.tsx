97c56633f5b98071018675969d0ab51008478b96
Toyehamzat <anibiseun@gmail.com>
2024-05-14 07:16:36 +0100

    refactor: Update ImageSlider component to improve UI and add project links

--- app/component/ImageSlider.tsx
+++ app/component/ImageSlider.tsx
@@ -10,6 +10,8 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
+import { Github, PackageOpen } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export function CarouselPlugin() {
   const plugin = React.useRef(
@@ -18,7 +20,7 @@ export function CarouselPlugin() {
 
   const spotlightProjects = {
     Bauble: {
-      title: "baulble",
+      title: "bauble",
       desc: "A student project collaboration platform.",
       techStack: "NEXT.JS, POSTGRESQL, PRISMA",
       link: "https://github.com/baubleproject/bauble.git",
@@ -35,7 +37,7 @@ export function CarouselPlugin() {
       image: "/image/appleology.jpg",
     },
     "Not-netflix": {
-      title: "not-netflex",
+      title: "not-netflix",
       desc: "A clone to netflix.",
       techStack: "NEXT.JS, MONGODB, PRISMA",
       link: "https://github.com/Toyehamzat/not-netflix.git",
@@ -48,42 +50,50 @@ export function CarouselPlugin() {
     <>
       <Carousel
         plugins={[plugin.current]}
-        className="w-[91%] mx-auto"
+        className="w-[90%] hidden md:block mx-auto"
         onMouseEnter={plugin.current.stop}
         onMouseLeave={plugin.current.reset}
       >
         <CarouselContent>
           {Object.values(spotlightProjects).map((project, index) => (
             <CarouselItem key={index}>
-              <DirectionAwareHover imageUrl={project.image}>
-                <div className=" rounded-xl max-w-xl h-[500px] pb-5">
-                  <div>
-                    <h2 className="text-4xl text-[ #ccd6f6] font-bold">
+              <div
+                className=" bg-cover bg-center rounded-3xl relative"
+                style={{ backgroundImage: `url(${project.image})` }}
+              >
+                <div className="block absolute inset-0 w-full h-full bg-black/40 z-10 transition duration-500" />
+                <div className=" relative flex justify-center z-30 items-end m-auto max-w-xl h-[500px] pb-5">
+                  <div className="flex flex-col justify-center items-center text-center">
+                    <h2 className="text-4xl text-[#ccd6f6] font-bold">
                       {project.title}
                     </h2>
-                    <p className="text-[#ccd6f6] text-[18px]">{project.desc}</p>
-                    <p className="text-[#ccd6f6] text-[18px]">
+                    <p className="text-[#ccd6f6] font-bold text-[11px] sm:text-[18px]">
+                      {project.desc}
+                    </p>
+                    <p className="text-[#64ffda] font-boldtext-[11px] sm:text-[18px]">
                       {project.techStack}
                     </p>
-                    <a
-                      href={project.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-500 hover:underline"
-                    >
-                      Source Code
-                    </a>
-                    <a
-                      href={project.open}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-500 hover:underline ml-4"
-                    >
-                      Open App
-                    </a>
+                    <div className="flex flex-row items-center text-center">
+                      <a
+                        href={project.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-[#ccd6f6] font-boldtext-[11px] sm:text-[18px] hover:underline"
+                      >
+                        <Github />
+                      </a>
+                      <a
+                        href={project.open}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-[#ccd6f6] font-boldtext-[11px] sm:text-[18px] hover:underline ml-4"
+                      >
+                        <PackageOpen />
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </DirectionAwareHover>
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
