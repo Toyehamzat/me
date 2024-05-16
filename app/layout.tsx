0f2f5f0874f21f62ac8cfea3db5c4b7eca2e115a
Toyehamzat <anibiseun@gmail.com>
2024-05-16 19:27:29 +0100

    refactor: Update layout metadata image URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,7 @@ export const metadata: Metadata = {
     title: "Anibi Adetoye Hamzat",
     description: "Portfolio",
     url: "https://tazmaheyot.vercel.app",
-    images:
-      "https://images.unsplash.com/photo-1581833971358-2c8b550f87b3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    images: "https://pin.it/7jS3cPWXp",
   },
 };
 export default function RootLayout({
