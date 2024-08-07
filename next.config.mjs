44b227ce69a7401aae0675f601d84aab83097544
Toyehamzat <anibiseun@gmail.com>
2024-08-07 14:31:30 +0100

    chore: Add webpack configuration for handling PDF files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+            publicPath: `/_next/static/files/`,
+            outputPath: `${isServer ? "../" : ""}static/files/`,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
