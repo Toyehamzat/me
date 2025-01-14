d8ab8398d6b2908260de4d9ce151b4ad5c3eb5cc
Toyehamzat <anibiseun@gmail.com>
2025-01-14 20:04:52 +0100

    refactor: Add StarsBackground component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,21 @@ import About from "./component/About";
 import Experience from "./component/Experience";
 import Education from "./component/Education";
 import PetProjects from "./component/PetProjects";
+import { StarsBackground } from "@/components/ui/stars-background";
 
 export default function Home() {
   return (
-    <TracingBeam className="px-6">
-      <main className="pt-10 z-50 max-w-screen-lg  m-auto">
-        <Intro />
-        <About />
-        <Experience />
-        <Education />
-        <PetProjects />
-      </main>
-    </TracingBeam>
+    <div className="relative w-full h-full">
+      <TracingBeam className="px-6">
+        <main className="pt-10 z-50 max-w-screen-lg  m-auto ">
+          <Intro />
+          <About />
+          <Experience />
+          <Education />
+          <PetProjects />
+        </main>
+      </TracingBeam>
+      <StarsBackground />
+    </div>
   );
 }
