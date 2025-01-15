cde54418d655d45407c90534a7b8a70f7f116ab7
Toyehamzat <anibiseun@gmail.com>
2025-01-15 14:23:39 +0100

    refactor: Remove TracingBeam and FollowingPointerCard components; simplify Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { TracingBeam } from "@/components/ui/tracing-beam";
 import Intro from "./component/Intro";
 import About from "./component/About";
 import Experience from "./component/Experience";
@@ -9,16 +8,14 @@ import { StarsBackground } from "@/components/ui/stars-background";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-full">
-      <TracingBeam className="px-6">
-        <main className="pt-10 z-50 max-w-screen-lg  m-auto ">
-          <Intro />
-          <About />
-          <Experience />
-          <Education />
-          <PetProjects />
-        </main>
-      </TracingBeam>
+    <div className="relative h-full">
+      <main className="pt-10 z-50 max-w-screen-lg m-auto px-6 ">
+        <Intro />
+        <About />
+        <Experience />
+        <Education />
+        <PetProjects />
+      </main>
       <StarsBackground />
     </div>
   );
